aa78fcf4175b69378c6d2b5ef28f414d7ac69fd5
CASSANDRA RAFAELA CORBANI <cassandra.corbani@alunos.sc.senac.br>
2025-04-11 20:21:29 -0300

    adicionando exercicio02

--- Entra 21/src/Exercício02.java
+++ Entra 21/src/Exercício02.java
@@ -2,4 +2,12 @@ public class Exercício02 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-}
+    int n = 5;
+//        int soma = 0;
+//            for (int i = 0; i<= n; i++) {
+//               soma += i;
+//
+//                System.out.println(soma);
+//
+//            }
+            }
