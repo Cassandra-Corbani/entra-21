a645f35f036eaf955ffca9eed358bae042f284a3
CASSANDRA RAFAELA CORBANI <cassandra.corbani@alunos.sc.senac.br>
2025-04-11 20:34:54 -0300

    adicionando exercicio03

---
+++ Entra 21/src/Exercício03.java
@@ -0,0 +1,12 @@
+public class Exercício03 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    int tab = 6;
+//            for (int i = 0; i <= 10; i++) {
+//                System.out.println(tab + "x" + i + "=" + (tab * i));
+//            }
+//
+//
+//            }
+            }
